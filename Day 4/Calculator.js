4e6d77f30f8d87261bb0c4bae022ce8be936eaf7
karthi-49 <81973165+karthi-49@users.noreply.github.com>
2026-02-04 10:13:30 +0530

    Function Day 4

---
+++ Day 4/Calculator.js
@@ -0,0 +1,34 @@
+function add()
+{
+    let i=10,j=20;
+    console.log(i+j);
+}
+
+function sub(i,j)
+{
+    console.log(i-j);
+}
+
+function multiply(i=20,j=10)
+{
+    console.log(i*j);
+}
+
+function divide(i,j)
+{
+    return i/j;
+}
+
+function percentage()
+{
+    let i=20, j=13;
+    return i%j;
+}
+
+add();
+sub(35,20)
+multiply(45)
+let Divide=divide(25,5)
+let percent=percentage()
+console.log(Divide)
+console.log(percent);
---
+++ Day 4/Methods.js
@@ -0,0 +1,32 @@
+
+function add()
+{
+    let i=10
+    let j=20
+
+    let sum= i+j
+    console.log(sum);
+    
+}
+
+function add_1(i,j)
+{
+
+    let sum= i+j
+    console.log(sum);
+    
+}
+function add_2(i=60,j=70)
+{
+
+    let sum= i+j
+    console.log(sum);
+    
+}
+add()
+add_1(5,6)
+add_2()
+add_2(30)
+add_2(40,90)
+
+
